ed222585693d8a0daef9f9d5cf57142232789197
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2025-11-02 21:13:12 +0530

    feat: added CORS and docker file for production

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ import pool from './db.js';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL , credentials: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
